e218ce1d4021461bff468579ba0bb7e3171c0bb9
KimKim168 <kimsoreya093@gmail.com>
2025-12-26 10:19:27 +0700

    dd

--- resources/js/pages/Sesor/components/navbars/logo.tsx
+++ resources/js/pages/Sesor/components/navbars/logo.tsx
@@ -4,12 +4,8 @@ export const Logo = () => {
     const { website_info } = usePage<any>().props;
 
     return (
-        <Link href='/' className="flex items-center gap-2">
-            <img
-                src={`/assets/images/website_infos/${website_info?.logo}`}
-                alt="Logo"
-                className="h-32 md:h-44 object-contain"
-            />
+        <Link href="/" className="flex items-center gap-2">
+            <img src={`/assets/images/website_infos/${website_info?.logo}`} alt="Logo" className="h-32 object-contain md:h-44" />
         </Link>
     );
 };
