60e8fcc140db7a0b53eb7fcae28f4fbabc75bfbf
KimKim168 <kimsoreya093@gmail.com>
2026-01-03 12:46:48 +0700

    cc

--- resources/js/pages/Sesor/components/LanguageSwitcher.tsx
+++ resources/js/pages/Sesor/components/LanguageSwitcher.tsx
@@ -10,7 +10,7 @@ export function LanguageSwitcher() {
                 window.location.href = `/lang/${val}`; // redirect to language route
             }}
         >
-            <SelectTrigger className="text-base items-center font-semibold justify-center text-white border-none shadow-none [&>svg]:text-white [&_svg]:w-4 [&_svg]:h-0 gap-1">
+            <SelectTrigger className="text-base items-center font-semibold justify-center text-white px-0 border-none shadow-none [&>svg]:text-white [&_svg]:w-4 [&_svg]:h-0 gap-1">
             <Globe className='text-white w-8 h-8 mr-1' size={10}/>
                 <SelectValue placeholder="Select language"/>
             <img src="/assets/PolyLine.png" className="w-3 mt-1 transition-transform duration-300" />
